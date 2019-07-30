7e232b1b88ce5c1d2bcdadd80ce1528241e86a74
Alexey Timoshenko <atimoshenko@payneteasy.com>
2019-07-30 14:17:41 +0300

    added boolean type to StartupParameters

---
+++ src/test/java/com/payneteasy/startup/parameters/IStartupConfig.java
@@ -0,0 +1,18 @@
+package com.payneteasy.startup.parameters;
+
+import java.io.File;
+
+public interface IStartupConfig {
+
+    @AStartupParameter(name = "WEB_SERVER_PORT", value = "8080")
+    int webServerPort();
+
+    @AStartupParameter(name = "WEB_SERVER_CONTEXT", value = "/")
+    String webServerContext();
+
+    @AStartupParameter(name = "UPLOAD_DIR", value = "./uploadDir")
+    File getUploadDir();
+
+    @AStartupParameter(name = "USE_DEBUG_MODE", value = "true")
+    boolean useDebugMode();
+}
